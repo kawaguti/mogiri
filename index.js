e42242f95b6214aa911c3ebc3218cbb193b434e4
sasakendayo <sasakendayo@gmail.com>
2022-04-14 17:12:33 +0900

    check evenbrite
    check evenbrite

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, eventbrite_private_key } = require('./config.json');
+const axios = require('axios');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -25,7 +26,27 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
     } else if (commandName === 'devopsdays') {
 		await interaction.reply(`Welcome to DevOpsDays Tokyo: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour Order Number: ${interaction.options.getString('ordernumber')}`);
-	}
+        const re = /#(\d{10})([^\d]|$)/;
+        if (( match_strings = re.exec(interaction.options.getString('ordernumber'))) !== null) {
+            const eventbrite_order_id = match_strings[1];
+            axios.get('https://www.eventbriteapi.com/v3/orders/'
+                    + eventbrite_order_id,
+                    { headers: {
+                        Authorization: `Bearer ${eventbrite_private_key}`,
+                    }
+            })
+            .then(function (response) {
+                // if ( isForThisEvent(message, eventbrite_order_id, response) &&
+                //     isValidOrderOnEventbrite(message, eventbrite_order_id, response)) {
+                //     setDiscordRole(message);
+                // }
+                console.log('okmaru!');
+            })
+            .catch(function (error) {
+                messageNotFoundOnEventbrite(message, eventbrite_order_id, error);
+            })
+        }
+    }
 });
 
 // Login to Discord with your client's token
