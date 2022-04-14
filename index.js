b8de261325858bfa1898321bd79b4b109ce3604d
sasakendayo <sasakendayo@gmail.com>
2022-04-14 15:48:19 +0900

    add login
    https://discordjs.guide/creating-your-bot/#creating-the-main-file

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
