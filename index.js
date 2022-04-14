8eb1216646bff3c5ff84fdc833e9088d0467d409
sasakendayo <sasakendayo@gmail.com>
2022-04-14 16:09:29 +0900

    add ping
    https://discordjs.guide/creating-your-bot/creating-commands.html#command-deployment-script

--- index.js
+++ index.js
@@ -10,5 +10,19 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 // Login to Discord with your client's token
 client.login(token);
