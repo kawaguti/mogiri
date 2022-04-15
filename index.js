970a42c804b809ae597118a3220a822a3f7a47ac
Yasunobu Kawaguchi <kawaguti@gmail.com>
2022-04-15 12:50:18 +0900

    refactoring

--- index.js
+++ index.js
@@ -13,29 +13,44 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
+	const { commandName } = interaction;
 
-    const { commandName } = interaction;
-    const ordernumber = interaction.options.getString('ordernumber');
-	console.log(ordernumber);
-
+	// mogiri
 	if (commandName === 'mogiri') {
 		await interaction.reply('ワタシハmogiriデス。って機械かーい！');
-    } else if (commandName === 'devopsdays') {
-        const channelId = interaction.channelId;
+		return;
+	}
+
+	// devopsdays
+	if (commandName === 'devopsdays') {
+		const channelId = interaction.channelId;
         const member = interaction.member;
-        const role = interaction.guild.roles.cache.find(role => role.name === discord_role);
-        if (role) {
-            if (interaction.member.roles.cache.some(role => role.name === discord_role)) {
-                client.channels.cache.get(channelId).send("すでに" + role.name + "のロールをお持ちでした！");
-            }
-        } else {
-            client.channels.cache.get(channelId).send(discord_role + "のロールがサーバー上に見つかりませんでした");
-        }
-        await interaction.reply(`Welcome to DevOpsDays Tokyo: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour Order Number: ${interaction.options.getString('ordernumber')}`);
-        const re = /#(\d{10})([^\d]|$)/;
-        if (( match_strings = re.exec(interaction.options.getString('ordernumber'))) !== null) {
-            const eventbrite_order_id = match_strings[1];
-            axios.get('https://www.eventbriteapi.com/v3/orders/'
+    	const ordernumber = interaction.options.getString('ordernumber');
+
+		// check roles
+		const role = interaction.guild.roles.cache.find(role => role.name === discord_role);
+		if (role == "") {
+            await interaction.reply(discord_role + "のロールがサーバー上に見つかりませんでした");
+			return;
+		} else if (interaction.member.roles.cache.some(role => role.name === discord_role)) {
+			await interaction.reply("すでに" + role.name + "のロールをお持ちでした！");
+			return;
+		}
+
+		// check eventbrite_order_id format
+		const re = /#(\d{10})([^\d]|$)/;
+		if ( re.exec(ordernumber) == null) {
+			await interaction.reply(ordernumber + "はEventbriteオーダー番号の形式ではありません。");
+			return;
+		}
+
+		// capture eventbrite_order_id
+		const eventbrite_order_id = re.exec(ordernumber)[1];
+		await interaction.reply("Asking for Eventbrite: " + eventbrite_order_id);
+
+		// asking for eventbrite
+		if ( eventbrite_order_id ) {
+			axios.get('https://www.eventbriteapi.com/v3/orders/'
                     + eventbrite_order_id,
                     { headers: {
                         Authorization: `Bearer ${eventbrite_private_key}`,
@@ -43,18 +58,14 @@ client.on('interactionCreate', async interaction => {
             })
             .then(function (response) {
                 if (response.data.event_id == eventbrite_event_id) {
-                    console.log('okmaru!');
                     client.channels.cache.get(channelId).send(eventbrite_order_id + "は有効なEventbriteオーダー番号です。")
                     member.roles.add(role);
                     client.channels.cache.get(channelId).send(role.name + "のロールをつけました！");
                 }
             })
             .catch(function (error) {
-                console.log('damemaru!');
                 client.channels.cache.get(channelId).send(eventbrite_order_id + "は有効なEventbriteオーダー番号ではありませんでした。")
             })
-        } else {
-            client.channels.cache.get(channelId).send(interaction.options.getString('ordernumber') + "はEventbriteオーダー番号の形式ではありません。")
         }
     }
 });
