d3a3fd8ff508ceb898f82b4073237891ca0a6344
Yasunobu Kawaguchi <kawaguti@gmail.com>
2022-05-06 18:18:27 +0900

    Revert "refactoring : isolate eventbrite request"
    This reverts commit 205dae1dd3b9c7387b8c974f5928f6af1e4e2487.

--- src/mogiri.js
+++ src/mogiri.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const { conferences } = require('../config.json');
+const axios = require('axios');
 const orderid = require('./orderid.js');
-const { eventbrite } = require("./eventbrite");
 
 module.exports = (client, interaction, ordernumber, member, conference_name ) => {
     const channelId = interaction.channelId;
@@ -24,9 +24,27 @@ module.exports = (client, interaction, ordernumber, member, conference_name ) =>
     }
 
     // asking for eventbrite
-    eventbrite(eventbrite_order_id, conference_name, client, channelId, member, role);
+    axios.get('https://www.eventbriteapi.com/v3/orders/'
+            + eventbrite_order_id,
+            { headers: {
+                Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+            }
+    })
+    .then(function (response) {
+        if (response.data.event_id == conferences[conference_name].eventbrite_event_id) {
+            // for this event
+            client.channels.cache.get(channelId).send(eventbrite_order_id + "は有効なEventbriteオーダー番号です。")
+            member.roles.add(role);
+            client.channels.cache.get(channelId).send(role.name + "のロールをつけました！");
+        } else {
+            // not for this event
+            client.channels.cache.get(channelId).send(eventbrite_order_id + "は別のイベントのオーダー番号のようです。 https://www.eventbrite.com/e/" + response.data.event_id)
+        }
+    })
+    .catch(function (error) {
+        // invalid order id
+        client.channels.cache.get(channelId).send(eventbrite_order_id + "は有効なEventbriteオーダー番号ではありませんでした。")
+    })
 
     return "Asking for Eventbrite: " + eventbrite_order_id;
 }
-
-
