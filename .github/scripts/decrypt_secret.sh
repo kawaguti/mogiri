20dd2ac996537a5a78c175ea84fb6f20300f0f57
Yasunobu Kawaguchi <kawaguti@gmail.com>
2022-06-09 14:12:57 +0900

    fix path error

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/config.json $HOME/secrets/config.json.gpg
+--output ./config.json ./secrets/config.json.gpg
