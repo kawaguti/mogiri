933566f7fc74b75e85d5483a3bbda57cc6d1c22b
Yasunobu Kawaguchi <kawaguti@gmail.com>
2022-08-20 17:52:22 +0900

    Update valid order number in RSGT2023

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,7 +5,7 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
-    it('should be scrumfest-osaka if ordernumber is $ordernumber_for_test', function(){
+    it('should be scrumfest-mikawa if ordernumber is $ordernumber_for_test', function(){
         const conference_name = "scrumfest-mikawa";
         const eventbrite_order_id = conferences[conference_name].ordernumber_for_test;
         axios.get('https://www.eventbriteapi.com/v3/orders/'
@@ -18,4 +18,18 @@ describe('eventbrite', function(){
             response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
         });
     });
+    it('should be rsgt2023 if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "rsgt2023";
+        const eventbrite_order_id = "4347745859";
+        axios.get('https://www.eventbriteapi.com/v3/orders/'
+        + eventbrite_order_id,
+        { headers: {
+            Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+        }
+        })
+        .then(function (response) {
+            response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
+        });
+    });
+
 });
