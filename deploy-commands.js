a7ff4424a1b0ead5088190d089f1551cc115c86a
Yasunobu Kawaguchi <kawaguti@gmail.com>
2022-09-23 13:01:11 +0900

    Support multi Discord servers / guilds

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,41 +1,41 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token, conferences } = require('./config.json');
+const { clientId, token, guilds } = require('./config.json');
 
-
-const commands = [
-	new SlashCommandBuilder().setName('mogiri').setDescription('Replies with hi!'),
-	new SlashCommandBuilder().setName('mogiri-check').setDescription('Tests!'),
-];
-
-Object.keys(conferences).map(name => { 
+Object.keys(guilds).map ( name => {
 	console.log(name);
 
-	commands.push(
-		new SlashCommandBuilder().setName(name).setDescription('Replies with ' + name + 'Admission!')
-		.addStringOption(option =>
+	const guildId = guilds[name].guildId;
+	console.log(guildId);
+
+	guilds[name].conferences.map ( name => {
+		console.log(name);
+		const commands = [];
+		commands.push( new SlashCommandBuilder().setName('mogiri').setDescription('Replies with hi!'));
+		commands.push( new SlashCommandBuilder().setName('mogiri-check').setDescription('Tests!'));
+		commands.push(
+			new SlashCommandBuilder().setName(name).setDescription('Replies with ' + name + 'Admission!')
+			.addStringOption(option =>
+				option.setName('ordernumber')
+					.setDescription('Eventbrite Order Number')
+					.setRequired(true))
+		);
+		commands.push(
+			new SlashCommandBuilder().setName('welcome-' + name).setDescription('Replies with ' + name + 'Admission!')
+			.addStringOption(option =>
 			option.setName('ordernumber')
 				.setDescription('Eventbrite Order Number')
 				.setRequired(true))
-	);
-	commands.push(
-		new SlashCommandBuilder().setName('welcome-' + name).setDescription('Replies with ' + name + 'Admission!')
-		.addStringOption(option =>
-		option.setName('ordernumber')
-			.setDescription('Eventbrite Order Number')
-			.setRequired(true))
-		.addStringOption(option =>
-			option.setName('memberid')
-				.setDescription('Discord Member ID')
-				.setRequired(true))
-	);
+			.addStringOption(option =>
+				option.setName('memberid')
+					.setDescription('Discord Member ID')
+					.setRequired(true))
+		);
+		const commandsJson = commands.map(command => command.toJSON());
+		const rest = new REST({ version: '9' }).setToken(token);
+		rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsJson })
+				.then(() => console.log('Successfully registered application commands.'))
+				.catch(console.error);	
+	});
 });
-
-const commandsJson = commands.map(command => command.toJSON());
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsJson })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
---
+++ test/guilds.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const should  = require('should');
+const { conferences, guilds } = require('../config.json');
+
+describe('guildss', function(){
+    it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
+        guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
+    })
+
+    it('should be ScrumFestOsaka has rsgt2023 command', function(){
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt2023"]);
+    })    
+
+})
