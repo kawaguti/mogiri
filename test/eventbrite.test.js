67627d390aa1bbb6773fda47bf4abf20070b52e8
Aki <aki.onetime@gmail.com>
2022-11-22 23:16:00 +0900

    Update config.json : Sapporo released

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,19 +5,6 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
-    it('should be scrum-fest-sapporo if ordernumber is $ordernumber_for_test', function(){
-        const conference_name = "scrum-fest-sapporo";
-        const eventbrite_order_id = conferences[conference_name].ordernumber_for_test;
-        axios.get('https://www.eventbriteapi.com/v3/orders/'
-        + eventbrite_order_id,
-        { headers: {
-            Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
-        }
-        })
-        .then(function (response) {
-            response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
-        });
-    });
     it('should be rsgt2023 if ordernumber is $ordernumber_for_test', function(){
         const conference_name = "rsgt2023";
         const eventbrite_order_id = "4347745859";
--- test/guilds.test.js
+++ test/guilds.test.js
@@ -4,10 +4,6 @@ const should  = require('should');
 const { conferences, guilds } = require('../config.json');
 
 describe('guildss', function(){
-    it('should be ScrumFestSapporo has scrum-fest-sapporo command', function(){
-        guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
-    })
-
     it('should be ScrumFestOsaka has rsgt2023 command', function(){
         guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt2023"]);
     })    
