e42b06677751b998a91b10a3a43fd04406df3471
Aki <aki.onetime@gmail.com>
2023-02-13 23:06:25 +0900

    Release RSGT 2023 & Add Fukuoka 2023

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,9 +5,9 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
-    it('should be rsgt2023 if ordernumber is $ordernumber_for_test', function(){
-        const conference_name = "rsgt2023";
-        const eventbrite_order_id = "4347745859";
+    it('should be scrum-fest-fukuoka if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "scrum-fest-fukuoka";
+        const eventbrite_order_id = "5836579059";
         axios.get('https://www.eventbriteapi.com/v3/orders/'
         + eventbrite_order_id,
         { headers: {
--- test/guilds.test.js
+++ test/guilds.test.js
@@ -4,8 +4,8 @@ const should  = require('should');
 const { conferences, guilds } = require('../config.json');
 
 describe('guildss', function(){
-    it('should be ScrumFestOsaka has rsgt2023 command', function(){
-        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt2023"]);
-    })    
+    it('should be ScrumFestOsaka has scrum-fest-fukuoka command', function(){
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrum-fest-fukuoka"]);
+    })
 
 })
