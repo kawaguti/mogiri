fa09786541acd4f99c8463142aa3fa1c12f4f04f
Yasunobu Kawaguchi <kawaguti@gmail.com>
2023-06-23 22:07:56 +0900

    fix bug in deploy commands  and add scrum-fest-mikawa
    "deploy command" could only be used once per conference, so it has been modified so that multiple commands can be created.
    deploy command がカンファレンスごとに一個しかできなかったんで複数作れるように修正しました。

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,11 +9,12 @@ Object.keys(guilds).map ( name => {
 	const guildId = guilds[name].guildId;
 	console.log(guildId);
 
+	console.log(name);
+	const commands = [];
+	commands.push( new SlashCommandBuilder().setName('mogiri').setDescription('Replies with hi!'));
+	commands.push( new SlashCommandBuilder().setName('mogiri-check').setDescription('Tests!'));
+
 	guilds[name].conferences.map ( name => {
-		console.log(name);
-		const commands = [];
-		commands.push( new SlashCommandBuilder().setName('mogiri').setDescription('Replies with hi!'));
-		commands.push( new SlashCommandBuilder().setName('mogiri-check').setDescription('Tests!'));
 		commands.push(
 			new SlashCommandBuilder().setName(name).setDescription('Replies with ' + name + ' Admission!')
 			.addStringOption(option =>
@@ -32,10 +33,12 @@ Object.keys(guilds).map ( name => {
 					.setDescription('Discord Member ID')
 					.setRequired(true))
 		);
-		const commandsJson = commands.map(command => command.toJSON());
-		const rest = new REST({ version: '9' }).setToken(token);
-		rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsJson })
-				.then(() => console.log('Successfully registered application commands.'))
-				.catch(console.error);	
 	});
+
+	const commandsJson = commands.map(command => command.toJSON());
+	console.log(commandsJson);
+	const rest = new REST({ version: '9' }).setToken(token);
+	rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandsJson })
+			.then(() => console.log('Successfully registered application commands.'))
+			.catch(console.error);	
 });
--- test/guilds.test.js
+++ test/guilds.test.js
@@ -5,7 +5,7 @@ const { conferences, guilds } = require('../config.json');
 
 describe('guildss', function(){
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
-        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrumfest-osaka"]);
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrumfest-osaka", "scrumfest-mikawa"]);
     })
 
 })
