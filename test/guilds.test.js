a72f4a498f7afae8ab3d9fc648cf7cea47157e86
Yasunobu Kawaguchi <kawaguti@gmail.com>
2023-08-06 16:36:39 +0900

    add rsgt

--- test/guilds.test.js
+++ test/guilds.test.js
@@ -5,7 +5,7 @@ const { conferences, guilds } = require('../config.json');
 
 describe('guilds', function(){
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
-        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrumfest-sendai","scrumfest-mikawa"]);
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrumfest-sendai","scrumfest-mikawa","rsgt"]);
     })
     it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
         guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
