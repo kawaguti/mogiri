b0c8c522981cb2c1697a9e71e61accaa0563d857
Yasunobu Kawaguchi <kawaguti@gmail.com>
2023-08-09 22:46:51 +0900

    rels #30 fix /mogiri-check

--- src/mogiri-check.js
+++ src/mogiri-check.js
@@ -33,6 +33,7 @@ module.exports = (client, interaction, conference_name ) => {
             + eventbrite_order_id,
             { headers: {
                 Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
             }
     })
     .then(function (response) {
