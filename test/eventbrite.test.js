882c01278fa7e465297a139245af167c91cd41b9
Aki <aki.onetime@gmail.com>
2024-01-17 02:38:54 +0900

    Add women in agile tokyo, fukuoka . Remove mikawa, sendai

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,9 +5,22 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
-    it('should be scrumfest-sendai if ordernumber is $ordernumber_for_test', function(){
-        const conference_name = "scrumfest-sendai";
-        const eventbrite_order_id = "7307425449";
+    it('should be scrumfest-fukuoka if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "scrumfest-fukuoka";
+        const eventbrite_order_id = "8709431699";
+        axios.get('https://www.eventbriteapi.com/v3/orders/'
+        + eventbrite_order_id,
+        { headers: {
+            Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+        }
+        })
+        .then(function (response) {
+            response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
+        });
+    });
+    it('should be woman-in-agile if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "woman-in-agile";
+        const eventbrite_order_id = "8710421479";
         axios.get('https://www.eventbriteapi.com/v3/orders/'
         + eventbrite_order_id,
         { headers: {
--- test/guilds.test.js
+++ test/guilds.test.js
@@ -5,7 +5,7 @@ const { conferences, guilds } = require('../config.json');
 
 describe('guilds', function(){
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
-        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["scrumfest-sendai","scrumfest-mikawa","rsgt"]);
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt","woman-in-agile","scrumfest-fukuoka"]);
     })
     it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
         guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
