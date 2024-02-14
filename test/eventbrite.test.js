fc33326f29395a4415255ab99859b58f093cd1ac
Aki <aki.onetime@gmail.com>
2024-02-14 23:09:39 +0900

    Add scrum fest niigata.

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,6 +5,19 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
+    it('should be scrumfest-niigata if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "scrumfest-niigata";
+        const eventbrite_order_id = "8925555029";
+        axios.get('https://www.eventbriteapi.com/v3/orders/'
+        + eventbrite_order_id,
+        { headers: {
+            Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+        }
+        })
+        .then(function (response) {
+            response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
+        });
+    });
     it('should be scrumfest-fukuoka if ordernumber is $ordernumber_for_test', function(){
         const conference_name = "scrumfest-fukuoka";
         const eventbrite_order_id = "8709431699";
--- test/guilds.test.js
+++ test/guilds.test.js
@@ -5,7 +5,7 @@ const { conferences, guilds } = require('../config.json');
 
 describe('guilds', function(){
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
-        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt","woman-in-agile","scrumfest-fukuoka"]);
+        guilds["ScrumFestOsaka"].conferences.should.deepEqual(["rsgt","woman-in-agile","scrumfest-fukuoka","scrumfest-niigata"]);
     })
     it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
         guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
