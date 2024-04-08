f90fecf600270fa48f92cb196a859058985028b8
Aki <aki.onetime@gmail.com>
2024-04-08 22:47:55 +0900

    Add guild ScrumFest2024

--- test/guilds.test.js
+++ test/guilds.test.js
@@ -7,6 +7,9 @@ describe('guilds', function(){
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
         guilds["ScrumFestOsaka"].conferences.should.deepEqual(["devopsdays-tokyo","scrumfest-fukuoka","scrumfest-niigata"]);
     })
+    it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
+        guilds["ScrumFest2024"].conferences.should.deepEqual(["scrumfest-osaka"]);
+    })
     it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
         guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
     })
