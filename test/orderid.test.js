f3035c6494b60acb04a1996d98df118b00a27632
Aki <aki.onetime@gmail.com>
2024-07-12 23:40:03 +0900

    Updated ticket number processing to support 11-digit ticket numbers.

--- test/orderid.test.js
+++ test/orderid.test.js
@@ -17,9 +17,16 @@ describe('ordernumber', function(){
       orderid('#123456789').should.equal(false);
    });
    it('should be false if ordernumber is #12345678901', function(){
-      orderid('#12345678901').should.equal(false);
+      orderid('#12345678901').should.equal("12345678901");
    });
    it('should be false if ordernumber is 12345678901', function(){
-      orderid('12345678901').should.equal(false);
+      orderid('12345678901').should.equal("12345678901");
    });
+   it('should be false if ordernumber is #123456789012', function(){
+      orderid('#123456789012').should.equal(false);
+   });
+   it('should be false if ordernumber is 123456789012', function(){
+      orderid('123456789012').should.equal(false);
+   });
+
 });
