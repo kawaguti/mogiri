6b1e5548ee680128322c338dca4aea1d8a31c627
Aki <aki.onetime@gmail.com>
2024-10-08 01:27:56 +0900

    Add Okinawa

--- test/eventbrite.test.js
+++ test/eventbrite.test.js
@@ -5,6 +5,20 @@ const { conferences } = require('../config.json');
 const axios = require('axios');
 
 describe('eventbrite', function(){
+    it('should be scrumfest-okinawa if ordernumber is $ordernumber_for_test', function(){
+        const conference_name = "scrumfest-mikawa";
+        const eventbrite_order_id = "10655164469";
+        axios.get('https://www.eventbriteapi.com/v3/orders/'
+        + eventbrite_order_id,
+        { headers: {
+            Authorization: `Bearer ${conferences[conference_name].eventbrite_private_key}`,
+        }
+        })
+        .then(function (response) {
+            response.data.event_id.should.equal(conferences[conference_name].eventbrite_event_id);
+        });
+    });
+
     it('should be scrumfest-mikawa if ordernumber is $ordernumber_for_test', function(){
         const conference_name = "scrumfest-mikawa";
         const eventbrite_order_id = "10001466569";
