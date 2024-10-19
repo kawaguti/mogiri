2bcc5a1cd7c3724d7e247b0f433ace11f61aa735
Yasunobu Kawaguchi <kawaguti@gmail.com>
2024-10-19 17:12:29 +0900

    Add DevOpsDays Tokyo

--- test/guilds.test.js
+++ test/guilds.test.js
@@ -8,7 +8,7 @@ describe('guilds', function(){
         guilds["ScrumFestOsaka"].conferences.should.deepEqual([]);
     })
     it('should be ScrumFestOsaka has scrumfest-osaka command', function(){
-        guilds["ScrumFest2024"].conferences.should.deepEqual(["scrumfest-osaka", "scrumfest-kanazawa", "scrumfest-sendai", "scrumfest-mikawa", "scrumfest-okinawa", "rsgt"]);
+        guilds["ScrumFest2024"].conferences.should.deepEqual(["scrumfest-osaka", "scrumfest-kanazawa", "scrumfest-sendai", "scrumfest-mikawa", "scrumfest-okinawa", "rsgt", "devopsdays"]);
     })
     it('should be ScrumFestSapporo has scrumfest-sapporo command', function(){
         guilds["ScrumFestSapporo"].conferences.should.deepEqual(["scrum-fest-sapporo"]);
